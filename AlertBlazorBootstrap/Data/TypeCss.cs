79e34bcbab31da222dc8c616e107f9f157c7c904
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2020-08-05 16:55:18 -0300

    components alerts

---
+++ AlertBlazorBootstrap/Data/TypeCss.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlertBlazorBootstrap.Data
+{
+    public enum TypeCss
+    {
+        Primary,
+        Secondary,
+        Success,
+        Danger,
+        Warning,
+        Info,
+        Light,
+        Dark
+    }
+}
